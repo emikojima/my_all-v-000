919302066219bfa87e6ce310a4c003b340277bba
emi kojima <emi8emi@gmail.com>
2018-02-16 22:31:16 +0000

    automatically backed up by learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,10 +1,10 @@
 require 'pry'
 
 def my_all?(collection)
- block_return_values = []
- i = 0
+  block_return_values = []
+  i = 0
   while i < collection.length
-    block_return_values << yield (collection[i])
+    block_return_values << yield(collection[i])
     i += 1  
   end
   if block_return_values.include?(false)
